105b4c905305e24316281d9f9ee35021392ef668
Harish Hona <95570408+Harishhona07@users.noreply.github.com>
2024-01-17 23:25:30 +0545

    Update script.js

---
+++ los-pollos-hermanos/script.js
@@ -0,0 +1,6 @@
+ function showDialog() {
+          document.getElementById('dialogContainer').style.display = 'block';
+      }
+  function closeDialog() {
+          document.getElementById('dialogContainer').style.display = 'none';
+      }
