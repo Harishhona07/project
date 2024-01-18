aa4c5fc570137b1ef8ea74866b83b39dd13fdeab
Harish Hona <95570408+Harishhona07@users.noreply.github.com>
2024-01-18 21:16:03 +0545

    Update script.js

--- los-pollos-hermanos/script.js
+++ los-pollos-hermanos/script.js
@@ -5,9 +5,40 @@ function showDialog() {
 function closeDialog() {
     document.getElementById('dialogContainer').style.display = 'none';
 }
-function badGustavo() {
-    document.getElementById("gustavo-img").src = "assets/gus.png";
-}
-function goodGustavo() {
-    document.getElementById("gustavo-img").src="assets/gustavo.png"
-}
+
+var isHoverEnabled = true;
+function dangerItem() {
+    if (isHoverEnabled) {
+      var imgElement = document.getElementById("special-item");
+      imgElement.src = "assets/meth.jpg";
+      setTimeout(function () {
+        normalItem();
+      }, 500);
+
+      document.getElementById("special-itemDetails").innerHTML = "ERror! in tThe Srver $(&#%@G#*D&%#%@%@JD&F&#&(@*#!_#)+)&@T";
+      // Disable hover after the first hover
+      disableHover();
+    }
+  }
+
+  function normalItem() {
+    var imgElement = document.getElementById("special-item");
+    imgElement.src = null;
+    document.getElementById("special-itemDetails").innerHTML = "ERror! in tThe Srver $(&#%@G#*D&%#%@%@JD&F&#&(@*#!_#)+)&@T";
+    document.getElementById("menu").style.backgroundColor="black"
+    document.getElementById("about-us").style.backgroundColor="black"
+    document.getElementById("training").style.backgroundColor="black"
+    document.getElementById("gustavo-img").src="assets/gus.png";
+    document.getElementById("gustavo-img").style.backgroundImage="url('assets/methlab.jpg')";
+    document.getElementById("gustavo-img").style.backgroundSize="100% 100%";
+    document.getElementById("logo-img").src="assets/los-pollos-welcome-jumpsuit.png";
+    document.getElementById("logo-img2").src="assets/methcustomers.jpeg";
+  }
+
+  // Function to disable hover
+  function disableHover() {
+    isHoverEnabled = false;
+    var imgElement = document.getElementById("special-item");
+    imgElement.removeEventListener("mouseover", hoverImage);
+    imgElement.removeEventListener("mouseout", resetImage);
+  }
