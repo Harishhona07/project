bfbbce1acb8134a047d0e1a54bd8c6a13fe8d6d2
Harish Hona <95570408+Harishhona07@users.noreply.github.com>
2024-01-18 16:30:55 +0545

    Update script.js

--- los-pollos-hermanos/script.js
+++ los-pollos-hermanos/script.js
@@ -1,6 +1,13 @@
- function showDialog() {
-          document.getElementById('dialogContainer').style.display = 'block';
-      }
-  function closeDialog() {
-          document.getElementById('dialogContainer').style.display = 'none';
-      }
+function showDialog() {
+    document.getElementById('dialogContainer').style.display = 'block';
+
+}
+function closeDialog() {
+    document.getElementById('dialogContainer').style.display = 'none';
+}
+function badGustavo() {
+    document.getElementById("gustavo-img").src = "assets/gus.png";
+}
+function goodGustavo() {
+    document.getElementById("gustavo-img").src="assets/gustavo.png"
+}
